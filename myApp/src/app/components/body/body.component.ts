9d964231cb7444d50c1b49f66a01bc010f3eeb9d
Cesar Cruz <cesar.cruz182@gmail.com>
2019-05-17 14:47:42 -0500

    Aplicación básica con Angular

---
+++ myApp/src/app/components/body/body.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-body',
+    templateUrl: './body.component.html'
+})
+
+export class BodyComponent {
+
+    mostrar:boolean;
+    personas:string[];
+
+    constructor() {
+        this.mostrar = true;
+        this.personas = ['Cesar', 'Persona2'];
+    }
+}
